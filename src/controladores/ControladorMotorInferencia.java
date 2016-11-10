7f438628de7fb36c88ffb9f34ec1c5f7b5e3332f
Maiko Morales <maiko.moralesr@gmail.com>
2016-11-09 23:23:54 -0400

    Added engine controller

---
+++ src/controladores/ControladorMotorInferencia.java
@@ -0,0 +1,27 @@
+package controladores;
+
+import java.util.Iterator;
+import jess.Fact;
+import jess.JessException;
+import jess.Rete;
+
+public class ControladorMotorInferencia {
+    
+    private final Rete motorInferencia;
+    
+    public ControladorMotorInferencia() throws JessException {
+        motorInferencia = new Rete();
+        motorInferencia.batch("bc/templates.clp");
+        motorInferencia.batch("bc/hechos.clp");
+        motorInferencia.batch("bc/reglas.clp");
+    }
+    
+    public void ejecutar() throws JessException {
+        motorInferencia.run();
+        Iterator it = motorInferencia.listFacts();
+        while (it.hasNext()) {
+            Fact hecho = (Fact) it.next();
+            System.out.println(hecho);
+        }
+    }
+}
